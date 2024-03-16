7a04f3985ef3cabceb2ad572b25f5372b7d2bba4
cybenuki <danfeedev@gmail.com>
2024-03-16 13:25:21 -0500

    chores : init proyect

--- packages/discord_bot/src/index.ts
+++ packages/discord_bot/src/index.ts
@@ -1,23 +1,22 @@
-import {Client, Events, GatewayIntentBits  } from 'discord.js';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
 
-const bootstrap = async (client : Client<boolean>) => {
+const bootstrap = async () => {
+	const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+	client.once(Events.ClientReady, readyClient => {
+		console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	});
+
 	try {
 		await client.login(process.env.DISCORD_TOKEN);
 	} catch (error) {
-		console.error('Failed to login to discord', error, process.env.DISCORD_TOKEN);
+		console.error('Failed to login to discord', error);
 		process.exit(1);
 	}
-  };
+};
 
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
-
-
-bootstrap(client);
+bootstrap();
