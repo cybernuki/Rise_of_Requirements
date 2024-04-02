3d213e9c359d898398197f0a1fd986276e36c347
cybenuki <danfeedev@gmail.com>
2024-04-01 21:53:17 -0500

    feature (City Hall) : adapt chrequirements command to use the city service and reply user with a embed built by CityHallRequirementsEmbed

--- packages/discord_bot/src/commands/city-hall/ch-requiriments.command.ts
+++ packages/discord_bot/src/commands/city-hall/ch-requiriments.command.ts
@@ -1,8 +1,8 @@
-import { CacheType, ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CacheType, ChatInputCommandInteraction, MessagePayload, SlashCommandBuilder } from "discord.js";
 import { CommandInterface } from "../interface/command.interface";
-import city_hall_data from './city_hall.json';
+import { CityHallService } from "./services/city_hall.service";
+import { CityHallRequerimentsEmbed } from "../../embeds/city_hall_requierements.emebed";
 
-const choices = city_hall_data.map((level_req: { level: any; }) => { return { name: String(level_req.level), value: String(level_req.level) } });
 const chRequirementCommand: CommandInterface = {
   data: new SlashCommandBuilder()
     .setName('chrequeriments')
@@ -12,16 +12,16 @@ const chRequirementCommand: CommandInterface = {
         .setDescription('The requeriment to check')
         .setRequired(true)
         .addChoices(
-          ...choices
+          ...CityHallService.getChoices()
         )
     ),
 
   async execute(interaction: ChatInputCommandInteraction<CacheType>) {
     await interaction.deferReply({  ephemeral: true });
-    const city_hall = city_hall_data.find(city_hall => interaction.options.getString('level') === String(city_hall.level));
-    
-    const  payload = city_hall?.requirements.join(' ') || '';
-    await interaction.editReply(payload);
+    const cityHall = CityHallService.findCityHallByLevel(interaction.options.getString('level', true))
+  
+    const messagePayload : MessagePayload = new MessagePayload(interaction.user,{embeds: [...CityHallRequerimentsEmbed.getCityHallRequirements(cityHall)]});
+    await interaction.editReply(messagePayload);
   },
 };
 export default chRequirementCommand
