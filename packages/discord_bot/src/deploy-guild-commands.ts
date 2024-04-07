0793c1a604e9a631ddf4abc4633fda6cb7d813b7
cybenuki <danfeedev@gmail.com>
2024-04-07 01:20:23 -0500

    chores (guild deploy) : improve guild commands deploy to filter commands by guildOnly flag

--- packages/discord_bot/src/deploy-guild-commands.ts
+++ packages/discord_bot/src/deploy-guild-commands.ts
@@ -17,10 +17,11 @@ const rest = new REST().setToken(config.token);
 // and deploy your commands!
 (async () => {
 	try {
-		console.log(`Started refreshing ${commands.size} application (/) commands.`);
+		const guildCommands = commands.filter(value => value.guildOnly);
+		console.log(`Started refreshing ${guildCommands.size} application (/) commands.`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
-		const body = [...commands.mapValues(command => command.data.toJSON()).values()];
+		const body = [...guildCommands.mapValues(command => command.data.toJSON()).values()];
 		const data = await rest.put(
 			Routes.applicationGuildCommands(config.clientId, config.guildId),
 			{ body: body },
