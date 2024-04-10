02bb7cd86ef0bbd15db906d60cee456fa2361bda
cybenuki <danfeedev@gmail.com>
2024-04-10 01:36:30 -0500

    feature: implement new help command

---
+++ packages/discord_bot/src/commands/help/index.ts
@@ -0,0 +1,7 @@
+import { CommandInterface } from '../interface/command.interface';
+import helpCommand from './help.command';
+
+const helpCommands: CommandInterface[] = [helpCommand];
+
+export default helpCommands;
+
--- packages/discord_bot/src/commands/index.ts
+++ packages/discord_bot/src/commands/index.ts
@@ -1,6 +1,7 @@
 import { CommandInterface } from './interface/command.interface';
 import cityHallCommands from './city-hall';
 import pingCommands from './ping';
+import helpCommands from './help';
 
-const commands: CommandInterface[] = [...cityHallCommands, ...pingCommands];
+const commands: CommandInterface[] = [...cityHallCommands, ...pingCommands, ...helpCommands];
 export default commands;
