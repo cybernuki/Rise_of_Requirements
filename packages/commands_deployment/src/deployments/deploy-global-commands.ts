0f4eb99c40389f9cc0c459290ef65afa52187652
cybenuki <danfeedev@gmail.com>
2024-04-27 00:44:56 -0500

    feature (deployment) : Implement update logic to global commands

--- packages/commands_deployment/src/deployments/deploy-global-commands.ts
+++ packages/commands_deployment/src/deployments/deploy-global-commands.ts
@@ -1,12 +1,14 @@
 import { Collection, REST, Routes } from 'discord.js';
 import { CommandInterface } from 'rok_discord_bot/src/commands/interface/command.interface';
+import CommandService from 'src/services/command.service';
 
 
 // and deploy your commands!
 export const globalCommandsDeployment = async (
 	rest: REST,
 	commands: Collection<string, CommandInterface>,
-	clientId: string
+	clientId: string,
+	commandService: CommandService
 ) => {
 	try {
 		const globalCommands = commands.filter(value => !value.guildOnly);
@@ -18,6 +20,12 @@ export const globalCommandsDeployment = async (
 			Routes.applicationCommands(clientId),
 			{ body: body },
 		) as any[];
+
+		console.log(`Started updating command data to DB`);
+		const commandUpdated = data.map(item => { return { name: item.name, id: item.id } })
+		await commandService.updateCommands(commandUpdated);
+
+		console.log(`Successfully updated command data to DB`);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
--- packages/commands_deployment/src/main.ts
+++ packages/commands_deployment/src/main.ts
@@ -3,6 +3,9 @@ import { getCommandsCollection } from 'rok_discord_bot/dist/utils'
 import { cleanCommands, globalCommandsDeployment, guildCommandsDeployment } from './deployments';
 
 import dotenv from 'dotenv';
+import CommandRepository from './database/repository/command.repository';
+import ConfigInterface from './interfaces/config.interface';
+import CommandService from './services/command.service';
 dotenv.config();
 
 const config = {
@@ -11,11 +14,29 @@ const config = {
   token: process.env.DISCORD_TOKEN || '',
 };
 
+const dbConfig: ConfigInterface = {
+  accessKeyId: process.env.AWS_ACCESS_KEY || '',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+  region: process.env.AWS_REGION || '',
+  tableName: process.env.AWS_TABLE_NAME || '',
+};
+
 (async () => {
   const rest = new REST().setToken(config.token);
   const commands = getCommandsCollection();
+  const commandRepository = new CommandRepository(dbConfig);
+  const commandService = new CommandService(commandRepository);
 
   await cleanCommands(rest, config.clientId, config.guildId);
-  await globalCommandsDeployment(rest, commands, config.clientId)
-  await guildCommandsDeployment(rest, commands, config.clientId, config.guildId);
+  await globalCommandsDeployment(rest,
+    commands,
+    config.clientId,
+    commandService
+  )
+  await guildCommandsDeployment(
+    rest,
+    commands,
+    config.clientId,
+    config.guildId
+  );
 })()
