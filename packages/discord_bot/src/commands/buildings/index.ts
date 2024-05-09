cb379c0f8e920a15481dbcbbb4f4049852bc0733
Jhonatan Arenas <55113840+cybernuki@users.noreply.github.com>
2024-05-08 19:16:14 -0500

    Feature :  handle autocomplete commands (#20)
    * feature : handle auto complete commands
    
    * feature : create new event to handle autocomplete interactions
    
    * fix : include interaction in autocomplete call
    
    * feature : implement autocomplete command for buildings

---
+++ packages/discord_bot/src/commands/buildings/index.ts
@@ -0,0 +1,7 @@
+import { CommandInterface } from '../interface/command.interface';
+import BuildingsCommand from './buildings-requirements.command';
+
+const BuildingsCommands: CommandInterface[] = [BuildingsCommand];
+
+export default BuildingsCommands;
+
--- packages/discord_bot/src/events/interaction-create.event.ts
+++ packages/discord_bot/src/events/interaction-create.event.ts
@@ -7,9 +7,8 @@ declare module 'discord.js' {
 	}
 }
 
-export const interactionCreateEvent = {
+export const chatInputInteractionCreationEvent: EventInterface = {
 	name: Events.InteractionCreate,
-  once: false,
 	async execute(interaction:Interaction) {
 		if (!interaction.isChatInputCommand()) return;
 
@@ -21,7 +20,7 @@ export const interactionCreateEvent = {
 		}
 
 		try {
-			await command.execute(interaction);
+				await command.execute(interaction);
 		} catch (error) {
 			console.error(error);
 			if (interaction.replied || interaction.deferred) {
@@ -31,4 +30,25 @@ export const interactionCreateEvent = {
 			}
 		}
 	},
-} as EventInterface;
+};
+
+
+export const AutoCompleteInteractionCreationEvent: EventInterface = {
+	name: Events.InteractionCreate,
+	async execute(interaction:Interaction) {
+		if (!interaction.isAutocomplete()) return;
+
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
+
+		try {
+				await command.autocomplete(interaction);
+		} catch (error) {
+			console.error(error);
+		}
+	},
+};
